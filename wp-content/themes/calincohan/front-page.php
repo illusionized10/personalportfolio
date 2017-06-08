a71269f4eb4453a36041e79748260ca241e4880a
illusionized10 <colincohan10@gmail.com>
2017-06-08 10:49:27 -0700

    changes

--- wp-content/themes/calincohan/front-page.php
+++ wp-content/themes/calincohan/front-page.php
@@ -21,6 +21,31 @@ get_header(); ?>
 			Custom front page template
 		</div>
 
+		<div class="about-panel">
+			<div class="line"></div>
+			<div class="header">
+				<h2>About Calin Cohan</h2>
+				<h3>So, who is this guy?</h3>
+			</div>
+			<div class="content">
+				<div class="row">
+					<div class="container">
+						<div class="col-md-4">
+							<div class="left">
+								<p>I design and develop stunning websites to meet your business objectives. I won’t talk to you in technical jargon, I’ll listen to your needs, hatch the perfect plan, and create a product that gives you the best possible return on your investment.</p>
+							</div>
+						</div>
+						<div class="col-md-8">
+							<div class="right">
+								<p>I’m primarily a web designer and front-end developer, with a fountain of knowledge in Photoshop, Illustrator, Responsive HTML5, CSS3, jQuery, WordPress & eCommerce development. But I also have experience in branding and print design (business cards, flyers, brochures, posters etc.)</p>
+								<p>So if you’re a business in need of a new website, mobile app or brand refresh, or maybe you’re an agency in need of an extra pair of hands or some digital expertise – get in touch and we’ll have a chat – ideally over a coffee.</p>
+								<p>Do you need help with a web or digital project? Whether you just need an extra pair of digital hands, someone to cover those dastardly holidays, or maybe you need to get a guy in who ‘knows digital’ if you don’t have one in-house – I can help.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
